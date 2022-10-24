bbf8b548bb82a9bc514ffe687259f8bc5e0d7077
JuanCPoos <juan.cpoos@gmail.com>
2022-10-24 20:52:46 -0300

    Se agrega sonido licuadora y botón. Final

--- js/motorcito.js
+++ js/motorcito.js
@@ -1,4 +1,6 @@
 let estadoLicuadora = "apagado";
+let sonidoLicuadora = document.getElementById("blender-sound");
+let sonidoBotonLicuadora = document.getElementById("blender-button-sound");   
 let licuadora = document.getElementById("blender");
 licuadora.classList.remove("active");
 
@@ -6,12 +8,26 @@ licuadora.classList.remove("active");
 el respectivo estado tomado*/
 function controlarLicuadora() { 
     if (estadoLicuadora == "apagado") { 
-        estadoLicuadora = "encendido"; 
-        licuadora.classList.add("active"); 
+        estadoLicuadora = "encendido";         
+        controlarSonidoLicuadora();
+        licuadora.classList.add("active");                 
         /* console.log("me encendiste"); */
     } else {
         estadoLicuadora = "apagado";
+        controlarSonidoLicuadora();
         licuadora.classList.remove("active"); 
         /* console.log("me apagaste"); */
+    }
+}
+
+function controlarSonidoLicuadora() {
+    if (sonidoLicuadora.paused) {
+        sonidoBotonLicuadora.play();
+        sonidoLicuadora.play();
+    }
+    else { 
+        sonidoBotonLicuadora.play();
+        sonidoLicuadora.pause();
+        sonidoLicuadora.currentTime = 0;
     }
 }
